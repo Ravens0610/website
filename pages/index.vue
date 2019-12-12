b99e607d12fe5e7b6999b717d5bbcfcf69aabac9
Tristan Ross <spaceboyross@yandex.com>
2019-12-11 22:32:05 -0800

    Added code

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Eternal Dream Project
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Soon this page will be filled with newly uploaded videos and
+            community circle posts. Right now, many features are either not
+            implemented or work-in-progress.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {}
+</script>
